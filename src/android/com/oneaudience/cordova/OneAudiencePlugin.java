f7d0f7922db7b28d5bc91533a6f336710ff19734
Mor Amedi <mor@oneaudience.com>
2017-02-08 13:00:22 +0200

    Updated iOS and android sdks to latest versions. iOS 5.5 android 5.5.1.9

--- src/android/com/oneaudience/cordova/OneAudiencePlugin.java
+++ src/android/com/oneaudience/cordova/OneAudiencePlugin.java
@@ -12,12 +12,18 @@ import org.json.JSONException;
 
 public class OneAudiencePlugin extends CordovaPlugin {
     public static final String ACTION_INIT = "init";
+    public static final String ACTION_REQUEST_ACCOUNT_PERMISSION = "requestAccountPermission";
+    public static final String ACTION_OPTOUT = "optout";
 
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals(ACTION_INIT)) {
-            OneAudience.init(cordova.getActivity().getApplication(), args.getString(0));
+            OneAudience.init(cordova.getActivity(), args.getString(0));
+        } else if (action.equals(ACTION_REQUEST_ACCOUNT_PERMISSION)){
+        	OneAudience.requestAccountPermission(cordova.getActivity());
+        } else if (action.equals(ACTION_OPTOUT)){
+        	OneAudience.optout();
         } else {
             callbackContext.error("OneAudienceCordova: " + action + " is not supported");
             return false;
