65a1c3e7de947553a3bd8793b3128ddf1ffb551e
Mor Amedi <mor@oneaudience.com>
2017-02-27 14:39:32 +0200

    no message

--- www/oneaudiencecordova.js
+++ www/oneaudiencecordova.js
@@ -17,10 +17,10 @@ OneAudienceCordova.prototype.init = function(appKey, successCallback, errorCallb
 OneAudienceCordova.prototype.optout = function(successCallback, errorCallback) {
 	var devicePlatform = device.platform;
 	if (devicePlatform === "Android") {
-		cordova.exec(successCallback, this._getErrorCallback(errorCallback, "optout"), "OneAudiencePlugin", "optout", null); 
+		cordova.exec(successCallback, this._getErrorCallback(errorCallback, "optout"), "OneAudiencePlugin", "optout", []); 
 	} else if (devicePlatform === "iOS") {
 		console.log("OneAudience Init IOS");
-		cordova.exec(successCallback, this._getErrorCallback(errorCallback, "optOut"), "OneAudienceCordova", "optOut", null); 
+		cordova.exec(successCallback, this._getErrorCallback(errorCallback, "optOut"), "OneAudienceCordova", "optOut", []); 
 	}
 	
 };
@@ -39,7 +39,7 @@ OneAudienceCordova.prototype.setEmailAddress = function(email, successCallback, 
 OneAudienceCordova.prototype.requestAccountPermission = function(emailsuccessCallback, errorCallback) {
 	var devicePlatform = device.platform;
 	if (devicePlatform === "Android")
-		cordova.exec(successCallback, this._getErrorCallback(errorCallback, "requestAccountPermission"), "OneAudiencePlugin", "requestAccountPermission", null); 
+		cordova.exec(successCallback, this._getErrorCallback(errorCallback, "requestAccountPermission"), "OneAudiencePlugin", "requestAccountPermission", []); 
 	
 };
 
