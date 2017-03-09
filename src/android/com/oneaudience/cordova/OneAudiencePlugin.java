e13c8bb795bbfb8379df46ed4a96d9d277423640
miha93 <miha93@users.noreply.github.com>
2017-03-09 12:40:29 +0200

    Revert "Updated iOS and android sdks to latest versions."

--- src/android/com/oneaudience/cordova/OneAudiencePlugin.java
+++ src/android/com/oneaudience/cordova/OneAudiencePlugin.java
@@ -12,39 +12,18 @@ import org.json.JSONException;
 
 public class OneAudiencePlugin extends CordovaPlugin {
     public static final String ACTION_INIT = "init";
-    public static final String ACTION_REQUEST_ACCOUNT_PERMISSION = "requestAccountPermission";
-    public static final String ACTION_OPTOUT = "optout";
-    public static final String ACTION_SET_EMAIL = "setEmailAddress";
 
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        
-        final String _action = action;
-        final CallbackContext _callbackContext = callbackContext;
+        if (action.equals(ACTION_INIT)) {
+            OneAudience.init(cordova.getActivity().getApplication(), args.getString(0));
+        } else {
+            callbackContext.error("OneAudienceCordova: " + action + " is not supported");
+            return false;
+        }
 
-        final String param = (args != null && args.length() > 0) ? args.getString(0) : ""; 
-
-        cordova.getActivity().runOnUiThread(new Runnable() {
-
-            public void run() {
-                if (_action.equals(ACTION_INIT)) {
-                    OneAudience.init(cordova.getActivity(), param);
-                } else if (_action.equals(ACTION_REQUEST_ACCOUNT_PERMISSION)){
-                    OneAudience.requestAccountPermission(cordova.getActivity());
-                } else if (_action.equals(ACTION_OPTOUT)){
-                    OneAudience.optOut();
-                } else if (_action.equals(ACTION_SET_EMAIL)){
-                    OneAudience.setEmailAddress(param);
-                } else {
-                    _callbackContext.error("OneAudienceCordova: " + _action + " is not supported");
-                }
-
-                _callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
-            }
-
-        });
-
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
         return true;
     }
 
--- src/ios/OneAudienceCordova.m
+++ src/ios/OneAudienceCordova.m
@@ -8,9 +8,6 @@
 }
 
 - (void)init:(CDVInvokedUrlCommand*)command;
-- (void)setEmailAddress:(CDVInvokedUrlCommand*)command;
-- (void)optOut:(CDVInvokedUrlCommand*)command;;
-
 @end
 
 @implementation OneAudienceCordova
@@ -24,9 +21,7 @@
     if (appKey != nil && [appKey length] > 0) {
         NSLog(@"OneAudience AppKey: %@", appKey);
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:appKey];
-        [self.commandDelegate runInBackground:^{
-            [AJKit init:appKey];
-        }];
+        [AJKit initWithLaunchOptions:appKey];
     } else {
         NSLog(@"OneAudience AppKey is nill or empty");
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
@@ -35,31 +30,4 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-- (void)setEmailAddress:(CDVInvokedUrlCommand*)command
-{
-    NSLog(@"OneAudience setEmailAddress Objective C called");
-    CDVPluginResult* pluginResult = nil;
-    NSString* email = [command.arguments objectAtIndex:0];
-
-    if (email != nil && [email length] > 0) {
-        NSLog(@"email: %@", email);
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:email];
-        [AJKit setEmailAddress:email];
-    } else {
-        NSLog(@"email is nill or empty");
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-- (void)optOut:(CDVInvokedUrlCommand*)command
-{
-    NSLog(@"optout Objective C called");
-
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    [AJKit optOut];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
 @end
